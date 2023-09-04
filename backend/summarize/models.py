d3452b42d18e9be000d92dee5fb5be55f6f78a42
Muhammad Siddiqi <muhammad.siddiqi@curemd.com>
2023-09-04 18:30:01 +0500

    Finalized  by myside

--- backend/summarize/models.py
+++ backend/summarize/models.py
@@ -7,7 +7,25 @@ from django.contrib.auth.models import (
 
 
 class UserAccountManager(BaseUserManager):
+    """
+    Custom manager for the UserAccount model.
+    """
     def create_user(self, email, username, password=None, **extra_fields):
+        """
+               Creates and saves a new user with the given email, username, and password.
+
+               Args:
+                   email (str): The user's email address.
+                   username (str): The user's username.
+                   password (str): The user's password.
+                   **extra_fields: Additional fields to be saved in the user's profile.
+
+               Returns:
+                   UserAccount: The newly created user account.
+
+               Raises:
+                   ValueError: If email is not provided.
+               """
         if not email:
             raise ValueError("User must have an email")
         email = self.normalize_email(email)
@@ -17,6 +35,18 @@ class UserAccountManager(BaseUserManager):
         return user
 
     def create_superuser(self, username, email, password=None, **extra_fields):
+        """
+               Creates and saves a new superuser with the given username, email, and password.
+
+               Args:
+                   username (str): The superuser's username.
+                   email (str): The superuser's email address.
+                   password (str): The superuser's password.
+                   **extra_fields: Additional fields to be saved in the superuser's profile.
+
+               Returns:
+                   UserAccount: The newly created superuser account.
+               """
         user = self.create_user(username, email, password=password, **extra_fields)
         user.is_active = True
         user.is_staff = True
@@ -26,6 +56,9 @@ class UserAccountManager(BaseUserManager):
 
 
 class UserAccount(AbstractBaseUser, PermissionsMixin):
+    """
+    Custom UserAccount model.
+    """
     email = models.EmailField(max_length=255, unique=True)
     username = models.CharField(max_length=255, unique=True)
     first_name = models.CharField(max_length=255)
@@ -40,19 +73,36 @@ class UserAccount(AbstractBaseUser, PermissionsMixin):
     REQUIRED_FIELDS = ["email", "first_name", "last_name"]
 
     def get_full_name(self):
+        """
+        Returns the user's full name.
+
+        Returns:
+            str: The user's full name in the format "first_name - last_name".
+        """
         return f"{self.first_name} - {self.last_name}"
 
     def get_short_name(self):
+        """
+        Returns the user's short name.
+
+        Returns:
+            str: The user's username.
+        """
         return self.username
 
     def __str__(self):
+        """
+        Returns a string representation of the user account.
+
+        Returns:
+            str: The user's email address.
+        """
         return self.email
 
 
 class Prompts(models.Model):
     """
-    Prompts model to store history of prompts issued by user
-    It includes: user, summary returned by chatgpt, patient id, date, and finally the current date which is automatically populated
+    Prompts model to store history of prompts issued by the user.
     """
 
     user = models.ForeignKey(UserAccount, on_delete=models.PROTECT, related_name='prompts')
